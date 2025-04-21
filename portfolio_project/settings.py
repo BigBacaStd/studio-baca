7b12d9de523e6a9a8d3cdc36fbece2532dc3f251
PoloBaca <thebigbaca@gmail.com>
2025-04-20 20:58:49 -0600

    commit April 20 2025

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&bq93-+&yp))ey&i4s*z^&_r+o6@u5p^q7%cxt#r=6##ms^mys
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.3.5']
 
 
 # Application definition
